bc3e9a0d46b801f3808edd67d114fabacd3d0c55
Kelin2025 <kelin2025@yandex.ru>
2018-02-28 01:34:45 +0300

    Added methods variations and unified way to use them

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 export default function VueBusPark(Vue, busesList) {
   const buses = {}
 
+  const addBus = (name, bus) => {
+    buses[name] = bus
+    buses[name].on = buses[name].on || buses[name].addEventListener
+    buses[name].emit = bus.emit || bus.send
+  }
+
   const createControls = vm => {
     const $events = vm.$options.events
     const controls = {
       add(name, bus) {
         controls.listeners[name] = {}
-        buses[name] = bus
+        addBus(name, bus)
         if (!$events || !$events[name]) return
         Object.entries($events[name]).forEach(([evt, cb]) => {
           controls.listeners[name][evt] = (
@@ -28,7 +34,7 @@ export default function VueBusPark(Vue, busesList) {
   }
 
   Object.entries(busesList).forEach(([name, bus]) => {
-    buses[name] = bus
+    addBus(name, bus)
   })
 
   Vue.mixin({
@@ -51,3 +57,20 @@ export default function VueBusPark(Vue, busesList) {
     }
   })
 }
+
+export function SocketBus(url, options) {
+  const socket = new WebSocket(url, options)
+  return {
+    on(evt, cb) {
+      socket.addEventListener(evt, cb)
+      return function() {
+        socket.removeEventListener(evt, cb)
+      }
+    },
+    emit() {
+      socket.send.apply(socket, arguments)
+    }
+  }
+}
+
+export { default as Bus } from 'nanoevents'
